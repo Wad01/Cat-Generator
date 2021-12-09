c0ed911483a17c56c44bcee2b7f7f04bba06e4c5
MD Jawad Aksir <mdjawadaksir@MDs-MacBook-Air.local>
2021-12-09 23:23:25 +0600

    Cat Generator

---
+++ script.js
@@ -0,0 +1,8 @@
+// task 2
+function generateCat() {
+  let image = document.createElement("img");
+  let div = document.getElementById("flex-cat-gen");
+  image.src =
+    "https://thecatapi.com/api/images/get?format=src&type=gif&size=small";
+  div.appendChild(image);
+}
